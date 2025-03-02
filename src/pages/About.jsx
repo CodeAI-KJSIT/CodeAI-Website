ea8f911b680f1e218d5c6503e2c0bf198b26df5f
vedant hundare <141746439+vedanthundare@users.noreply.github.com>
2025-03-02 19:15:50 +0530

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,87 +1,146 @@
-import { motion, useScroll, useSpring } from "framer-motion";
-import Title from "../components/Title";
-import AnimatedText from "../components/AnimatedText";
-import GetStarted from "../components/GetStarted";
-import Section from "../components/Section";
-import visionImage from "/vision.png";
-import missionImage from "/mission.png";
-import Nav from "../components/Nav";
-import Footer from "../components/Footer";
+import React from "react";
+import { motion } from "framer-motion";
+import Spark from "/Frame (4).png";
+import Down from "/Frame (5).png";
+import Round from "/Frame (6).png";
+import Share from "/Frame (7).png";
+import Flag from "/Frame (8).png";
+import bgImage from "/Desktop - 10.png";
+import logo from "/logo (2).png";
 
 const About = () => {
-  const { scrollYProgress } = useScroll();
-  const scaleX = useSpring(scrollYProgress, { stiffness: 100, damping: 30 });
-  const content = [
-    "Build a thriving community of AI enthusiasts.",
-    "Provide resources and guidance for mastering cutting-edge AI tools and techniques.",
-    "Encourage creativity and collaboration through real-world projects.",
-    "Empower individuals to make meaningful contributions to AI and machine learning.",
-  ];
-
   return (
-    <div id="about" className="min-h-screen text-white p-8 ">
-      <Nav />
-      <motion.div
-        className="fixed top-0 left-0 right-0 h-2 bg-[#8375F3]"
-        style={{ scaleX }}
-      />
+    <section
+      style={{
+        backgroundImage: `url(${bgImage})`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+      }}
+      className="mt-0 py-20 bg-[#090119]"
+      id="about"
+    >
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 cursor-default">
+        <motion.h2
+          className="lg:text-3xl text-xl font-light font-custom2 text-white mb-[10vw] lg:mb-[3vw] text-center"
+          initial={{ opacity: 0, x: -50 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1 }}
+          viewport={{ once: true }}
+        >
+          What is <br />
+          <p className="text-white font-custom flex justify-center text-[8vw] lg:text-[3vw] pt-6">
+            <img src={Spark} className="h-5 w-5 lg:pb-1" alt="" />
+            CODEAI
+            <img
+              src={Down}
+              className="h-20 w-7 ml-2 lg:h-20 lg:w-24 lg:ml-2"
+              alt=""
+            />
+          </p>
+        </motion.h2>
 
-      <div className="flex flex-col items-center justify-center min-h-screen text-center">
-        <Title />
-        <p className="mt-16 md:text-2xl font-custom3 lg:px-[25vw]">
-          Welcome to <AnimatedText />, where creativity meets technology to
-          redefine the future of AI and machine learning...
-        </p>
-        <GetStarted
-          onClick={() => window.scrollTo({ top: 600, behavior: "smooth" })}
-        />
-      </div>
-
-      <Section
-        className="font-custom3"
-        content="To empower aspiring data scientists and machine learning enthusiasts by providing a structured, collaborative, and hands-on learning experience. We aim to foster innovation and prepare the next generation of AI leaders for the evolving landscape of technology."
-        image={visionImage}
-      />
-      <div className="flex justify-center font-custom3">
         <motion.div
-          className="flex flex-col md:flex-row items-center text-white p-8 rounded-lg shadow-lg my-10 text-center w-full max-w-6xl"
-          initial={{ opacity: 0 }}
-          whileInView={{ opacity: 1 }}
+          id="logo"
+          className="w-full flex justify-center items-center"
+          
+        >
+          <img
+            src={logo}
+            className="absolute lg:h-[50vw] lg:w-[80vw] h-[100vw] w-screen"
+            alt=""
+          />
+        </motion.div>
+
+        <motion.div
+          className="bg-gray-800 bg-opacity-50 p-8 rounded-lg backdrop-blur-sm"
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: true }}
           transition={{ duration: 1 }}
         >
-          <motion.img
-            src={missionImage}
-            alt="Our Mission"
-            className="w-1/2 md:w-1/3 m-4"
-            initial={{ opacity: 0, x: -100 }}
-            whileInView={{ opacity: 1, x: 0 }}
-            transition={{ duration: 1, ease: "easeOut" }}
-          />
-          <motion.div
-            className="w-full md:w-2/3 m-4 ml-12"
-            initial={{ opacity: 0, x: -100 }}
-            whileInView={{ opacity: 1, x: 0 }}
-            transition={{ duration: 1, ease: "easeOut", delay: 0.2 }}
+          <motion.p
+            className="text-gray-300 leading-relaxed mb-8 text-center lg:text-start lg:mx-10 text-[1rem] lg:text-[1.2rem] font-custom2"
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1 }}
+            viewport={{ once: true }}
           >
-            <h2 className="text-2xl sm:text-3xl md:text-4xl font-bold">
-              Our <span className="text-[#EC27D5] font-custom">Mission</span>
-            </h2>
-            {Array.isArray(content) ? (
-              <ul className="mt-4 text-left md:text-xl list-disc list-outside">
-                {content.map((item, index) => (
-                  <li className="leading-loose" key={index}>
-                    {item}
-                  </li>
-                ))}
-              </ul>
-            ) : (
-              <p className="mt-4 font-custom">{content}</p>
-            )}
-          </motion.div>
+            CodeAI is a dynamic student-driven committee committed to empowering
+            individuals with a passion for technology, artificial intelligence,
+            and machine learning. As a hub for innovation and creativity, we aim
+            to bridge the gap between theoretical learning and real-world
+            applications. Through workshops, hackathons, and collaborative
+            projects, CodeAI provides a vibrant platform for aspiring tech
+            enthusiasts to explore, learn, and grow together.
+          </motion.p>
+
+          <motion.h3
+            className="text-2xl flex justify-center lg:justify-start font-semibold text-white mb-4 text-center lg:text-start"
+            initial={{ opacity: 0, x: -30 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 1 }}
+            viewport={{ once: true }}
+          >
+            <img
+              src={Round}
+              className="h-5 w-4 absolute lg:left-6 left-12 font-custom2"
+              alt=""
+            />
+            Our <span className="text-[#7F0D96] font-custom">VISION</span>
+            <img src={Share} className="h-8 w-10 lg:ml-5" alt="" />
+          </motion.h3>
+
+          <motion.p
+            className="text-gray-300 leading-relaxed mb-8 text-center lg:text-start lg:mx-10 text-[1rem] lg:text-[1.2rem] font-custom2"
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1 }}
+            viewport={{ once: true }}
+          >
+            At CodeAI, we envision a future where innovation thrives, and
+            technology becomes a force for solving real-world problems. Our goal
+            is to empower aspiring data scientists and machine learning
+            enthusiasts by fostering a structured, hands-on, and collaborative
+            learning experience. We aim to prepare individuals for the
+            ever-evolving landscape of artificial intelligence, equipping them
+            with the skills and mindset to lead in this transformative era.
+          </motion.p>
+
+          <motion.h3
+            className="text-2xl flex justify-center lg:justify-start font-semibold text-white mb-4 text-center lg:text-start font-custom2"
+            initial={{ opacity: 0, x: -30 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 1 }}
+            viewport={{ once: true }}
+          >
+            <img
+              src={Round}
+              className="h-5 w-4 absolute lg:left-6 left-9"
+              alt=""
+            />
+            Our <span className="text-[#7F0D96] font-custom">MISSION</span>
+            <img src={Flag} className="h-8 w-10 lg:ml-5" alt="" />
+          </motion.h3>
+
+          <motion.p
+            className="text-gray-300 leading-relaxed text-center lg:text-start lg:mx-10 text-[1rem] lg:text-[1.2rem] font-custom2"
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1 }}
+            viewport={{ once: true }}
+          >
+            Our mission is to create a nurturing environment where creativity
+            and learning go hand in hand. We strive to provide opportunities for
+            members to build expertise in data science and AI through inspiring
+            events, mentorship, and practical exposure. By encouraging a culture
+            of collaboration and continuous growth, CodeAI seeks to inspire
+            individuals to push boundaries and drive impactful change in the
+            tech industry.
+          </motion.p>
         </motion.div>
       </div>
-      <Footer />
-    </div>
+    </section>
   );
 };
 
